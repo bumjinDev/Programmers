19862856bef87d412c56415c7db83ccc6ec0c01e
bumjinDev <bumjin.dev@gmail.com>
2024-07-31 23:39:38 +0900

    commit : 이진 탐색 트리 탐색 알고리즘 완료

--- src/Algorithm/Tree/BstTree.java
+++ src/Algorithm/Tree/BstTree.java
@@ -88,9 +88,7 @@ public class BstTree {
 	/* 중위 탐색 메소드 */
 	public static Node inOrderTraversal(int data, Node rootNode) {
 		
-		
-		System.out.println("inOrderTraversal() 실행!");
-		System.out.println("rootNode : " + rootNode.data);
+		System.out.println("inOrderTraversal() 실행!\n");
 		
 		/* 다음 것이 존재한다면 없는 지점, 즉 마지막 왼쪽 노드를 찾을 때 까지 재귀적으로 재 호출한다. */
 		if(rootNode.leftNode != null) {
@@ -98,23 +96,25 @@ public class BstTree {
 		}
 		/* 마지막 왼쪽 노드인 마지막 서브트리로서 기능하는 루트 노드를 찾으면 분기하는 지점,
 		 * !! 만약 현재 노드 내 데이터가 탐색 조건으로 주어진 'data' 와 동일한 노드를 찾았으면 그 다음 별도의 로직은 적용하지 않고. 지금까지 호출한 재귀 함수 스택을 없애면서 복귀를 한다.
-		 *
-		 **/
-		System.out.println();
-		System.out.println("디버깅 - data : " + data + ", rootNode.data : " + rootNode.data);
+		 */
+
 		
 		if(rootNode.data == data) {
 			
 			return rootNode;
 			
-		} else {	/* !! 마지막 레벨의 가장 왼쪽 노드에서조차 값 못 찾았을 경우에는 오른쪽 Node 가 있는지 확인한다. 만약에 존재한다면 해당 시점부터 왼쪽을 다시 탐색하는 것을 실행하기 위해서 현재 함수의 재귀적 호출을 재 실행하여 또 다시 마지막 왼쪽 노드에 닿을 때 까지,
-					즉 'if(rootNode.leftNode != null)' 이걸 실행 완료하는 것을 유도한다, 또한 중요한 점은 아래 else 문은 마지막 왼쪽 노드에서 우측 노드 탐색 결과로 null 이 나오면 단순 현재 재귀 함수 호출을 종료하기 위한 목적으로만 null을 반환할 것이고
-					이에 따라 이전 순차대로 실행된 재귀 함수 복귀 지점은 'return inOrderTraversal(data, rootNode.leftNode);' 이고 그러므로 이어서 복귀된 함수들이 재 실행하는 지점은 'if(rootNode.data == data)' 여기가 되므로(이 구문 확인하는
-					것 자체가 성능 낭비긴 하다.) 각 복귀 할 때마다 'if(inOrderTraversal(data, rootNode.rightNode) != null)	' 이 코드가 동일하게 실행되므로 각 노드별로 공통적으로 우측 노드가 존재하는지에 따라 분기 실행되도록 유도한다.
-					결과적으로 루트 노드에 도달했을 시 자연스럽게 루트 노드 또한 우측 노드를 확인함으로써 탐색을 한다. */
+		/* 1. 마지막 레벨의 가장 왼쪽 노드에서 값을 찾지 못한 경우, 오른쪽 노드가 있는지 확인한다. 
+		 * 2. 만약 오른쪽 노드가 존재한다면, 해당 시점부터 왼쪽을 다시 탐색하기 위해 현재 함수의 재귀적 호출을 다시 실행하여 마지막 왼쪽 노드에 도달할 때까지 반복한다.
+		 * 3. 'if(rootNode.leftNode != null)' 조건을 완료하도록 유도한다.
+		 * 4. 중요한 점은, 아래 else 문은 마지막 왼쪽 노드에서 오른쪽 노드를 탐색한 결과로 null이 나오면 단순히 현재 재귀 함수 호출을 종료하기 위한 목적으로만 null을 반환한다.
+		 * 5. 이에 따라 이전 순차대로 실행된 재귀 함수 복귀 지점은 'return inOrderTraversal(data, rootNode.leftNode);'가 된다.
+		 * 6. 그러므로 이어서 복귀된 함수들이 재 실행되는 지점은 'if(rootNode.data == data)'가 된다. (이 구문을 확인하는 것 자체가 성능 낭비이긴 하다.)
+		 * 7. 각 복귀할 때마다 'if(inOrderTraversal(data, rootNode.rightNode) != null)' 코드가 동일하게 실행되므로 각 노드별로 공통적으로 오른쪽 노드가 존재하는지에 따라 분기 실행되도록 유도한다.
+		 * 8. 결과적으로 루트 노드에 도달했을 때 자연스럽게 루트 노드 또한 오른쪽 노드를 확인함으로써 탐색을 완료한다.
+		 */
+			
+		} else {
 			if(rootNode.rightNode != null) {
-				
-				System.out.println("디버깅 - 우측 노드 존재 확인 ! " + rootNode.rightNode.data);
 				return inOrderTraversal(data, rootNode.rightNode);
 				
 			} else
@@ -122,9 +122,34 @@ public class BstTree {
 		}
 	}
 	
-	
 	/* 후위 탐색 메소드 */
-	
+	public static Node postOrderTraversal(int data, Node rootNode) {
+	    
+	    /* 마지막 왼측 노드인 마지막 서브트리로 판정되는 말단 노드 탐색. */
+	    if (rootNode.leftNode != null) {
+	        Node leftResult = postOrderTraversal(data, rootNode.leftNode);
+	        if (leftResult != null) {
+	            return leftResult;
+	        }
+	    }
+	    
+	    /* 말단 노드 및 재귀적으로 복귀하면서 복귀 될 때에 각 호출되어 있는 메소드 스택 내 rootNode(각 서브트리 내 루트 노드로 간주) 내 data 값을 우측 -> 좌측 -> 루트 노드 순으로 비교해야 되는데 이때 우측 노드가 있으면 이어서 왼쪽 노드 끝단 탐색 재 실행. */
+	    if (rootNode.rightNode != null) {
+	        Node rightResult = postOrderTraversal(data, rootNode.rightNode);
+	        if (rightResult != null) {
+	            return rightResult;
+	        }
+	    }
+	    
+	    /* 이제부터 복귀 시점 부터 data 를 확인. */
+	    if (rootNode.data == data) {
+	        return rootNode;
+	    }
+	    
+	    return null;
+	}
+
+
 	public static void main(String[] args) {
 		
 		Random rand = new Random();
@@ -148,7 +173,21 @@ public class BstTree {
 		Node result2 = BstTree.inOrderTraversal(secondNode.data, rootNode);
 		System.out.println("result2 : " + result2.data + ", secondNode : " + secondNode.data);
 		
-		Node thirdNode = new Node(70);
-		Node forthNode = new Node(80);
+
+		 Node thirdNode = new Node(70);
+		 BstTree.addTree(thirdNode, rootNode);
+
+		 Node forthNode = new Node(80);
+		 BstTree.addTree(forthNode, rootNode);
+
+		
+	    /* 후위 탐색 테스트 */
+	    System.out.println("세번째 찾기 시작!");
+	    Node result3 = BstTree.postOrderTraversal(thirdNode.data, rootNode);
+	    System.out.println("result3 : " + result3.data + ", thirdNode : " + thirdNode.data);
+
+	    System.out.println("네번째 찾기 시작!");
+	    Node result4 = BstTree.postOrderTraversal(forthNode.data, rootNode);
+	    System.out.println("result4 : " + result4.data + ", forthNode : " + forthNode.data);
 	}
 }
