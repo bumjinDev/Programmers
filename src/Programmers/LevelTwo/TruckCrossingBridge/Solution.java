0c4fc62336b1663088d0d96f71561f4ed2d3a9da
bumjinDev <bumjin.dev@gmail.com>
2024-08-26 17:41:21 +0900

    코딩테스트 연습 스택/큐 다리를 지나는 트럭

---
+++ src/Programmers/LevelTwo/TruckCrossingBridge/Solution.java
@@ -0,0 +1,24 @@
+package Programmers.LevelTwo.TruckCrossingBridge;
+
+/* 스택/큐 > 다리를 지나는 트럭 : 대기 중인 트럭들이 정해진 다리 위에서 지나갈 때의 최단 시간 초 구하기.
+ * 
+ * 
+ */
+public class Solution {
+
+	// bridge_length : 한 다리에 한꺼번에 지나갈 수 있는 트럭 수.
+	// weight : 한 다리 내 한꺼번에 지나갈 수 있는 트록 총 무게.
+	// [] truck_weights : 다리를 지나가야 되는 대기트럭으로 각 배열 내 값들을 각 트럭 별 무게(kg).
+	public int solution(int bridge_length, int weight, int[] truck_weights) {
+        
+		
+		
+		return 0;
+    }
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
