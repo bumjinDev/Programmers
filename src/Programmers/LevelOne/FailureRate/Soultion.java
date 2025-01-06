a6a3afb90a7158c7fd68554218889d95f4dfef8b
bumjinDev <bumjin.dev@gmail.com>
2025-01-06 17:53:37 +0900

    commit : programers

---
+++ src/Programmers/LevelOne/FailureRate/Soultion.java
@@ -0,0 +1,15 @@
+package Programmers.LevelOne.FailureRate;
+
+class Solution {
+	
+     public static int[] solution(int N, int[] stages) {
+       
+    	int[] answer = {};
+        return answer;
+    }
+    
+    public static void main(String[] args) {
+    	
+    	
+	}
+}
